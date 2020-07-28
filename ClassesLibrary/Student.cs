4e0ad0244aa8b4dbfae8b98933a797be6832f35d
Derek Sym <derek_sym@outlook.com>
2020-07-28 17:07:19 -0500

    finished problem 1

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+        //fields
+    {
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        //props
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public float Gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }
+
+        //ctors, fqctor first
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Gpa = gpa;
+        }
+
+        public Student() { }
+
+        //Methods (ToString())
+
+        public override string ToString()
+        {
+            return string.Format("First Name: {0}\nLast Name: {1}\nID: {2}\nGPA: {3}", FirstName, LastName, Id, Gpa);
+        }
+    }
+}
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesLibrary;
 
 namespace TesterProgram
 {
@@ -10,6 +11,16 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student();
+            s1.FirstName = "Johnny";
+            s1.LastName = "Johnson";
+            s1.Id = "543321";
+            s1.Gpa = 3;//couldnt enter in decimal places, why is that?
+            Console.WriteLine(s1);
+
+            Student s2 = new Student("Davie", "Davison", "12345", 4);
+            Console.WriteLine(s2);
         }
+        
     }
 }
