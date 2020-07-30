10b0f93b12d6dddb5d57b00b85320ae94cbf3537
Derek Sym <derek_sym@outlook.com>
2020-07-29 19:23:20 -0500

    finished book problem

---
+++ ClassesLibrary/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int NumberOfPages { get; set; }
+
+
+        public Book(string title, string author, int numberOfPages)
+        {
+            Title = title;
+            Author = author;
+            NumberOfPages = numberOfPages;
+        }
+
+        public Book()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} is by {1} and has {2} pages.",Title,Author,NumberOfPages);
+        }
+    }
+}
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -11,45 +11,45 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
-            Student s1 = new Student();
-            s1.FirstName = "Johnny";
-            s1.LastName = "Johnson";
-            s1.Id = "543321";
-            s1.Gpa = 3.3f;//couldnt enter in decimal places, why is that?
-            Console.WriteLine(s1);
+            //Student s1 = new Student();
+            //s1.FirstName = "Johnny";
+            //s1.LastName = "Johnson";
+            //s1.Id = "543321";
+            //s1.Gpa = 3.3f;//couldnt enter in decimal places, why is that?
+            //Console.WriteLine(s1);
 
-            Student s2 = new Student("Davie", "Davison", "12345", 4);
-            Console.WriteLine(s2);
+            //Student s2 = new Student("Davie", "Davison", "12345", 4);
+            //Console.WriteLine(s2);
 
-            Vehicle v1 = new Vehicle("Ford", "Fiesta", 1996, 2045);
-            Console.WriteLine(v1);
+            //Vehicle v1 = new Vehicle("Ford", "Fiesta", 1996, 2045);
+            //Console.WriteLine(v1);
 
-            Vehicle v2 = new Vehicle();
-            v2.Make = "Dodge";
-            v2.Model = "Intrepid";
-            v2.Year = 1998;
-            v2.Weight = 2929;
-            Console.WriteLine(v2);
+            //Vehicle v2 = new Vehicle();
+            //v2.Make = "Dodge";
+            //v2.Model = "Intrepid";
+            //v2.Year = 1998;
+            //v2.Weight = 2929;
+            //Console.WriteLine(v2);
 
-            Login L1 = new Login();
-            L1.UserName = "HOBOB";
-            L1.Password = "GOBHAB";
-            Console.WriteLine(L1);
+            //Login L1 = new Login();
+            //L1.UserName = "HOBOB";
+            //L1.Password = "GOBHAB";
+            //Console.WriteLine(L1);
 
-            Login L2 = new Login("Harry", "Harry's password");
-            Console.WriteLine(L2);
+            //Login L2 = new Login("Harry", "Harry's password");
+            //Console.WriteLine(L2);
 
-            ContactInfo c1 = new ContactInfo("1234 Mayflower Dr.", "Denver", "CO", "64601", "6460211", "Hoba@aol.com");
-            Console.WriteLine(c1);
+            //ContactInfo c1 = new ContactInfo("1234 Mayflower Dr.", "Denver", "CO", "64601", "6460211", "Hoba@aol.com");
+            //Console.WriteLine(c1);
 
-            ContactInfo c2 = new ContactInfo();
-            c2.StreetAddress = "2435 Rocky Rd.";
-            c2.City = "Belmont";
-            c2.State = "Me";
-            c2.Zip = "75758";
-            c2.Phone = "7578484";
-            c2.Email = "hoba2@yahoo.com";
-            Console.WriteLine(c2);
+            //ContactInfo c2 = new ContactInfo();
+            //c2.StreetAddress = "2435 Rocky Rd.";
+            //c2.City = "Belmont";
+            //c2.State = "Me";
+            //c2.Zip = "75758";
+            //c2.Phone = "7578484";
+            //c2.Email = "hoba2@yahoo.com";
+            //Console.WriteLine(c2);
 
             Customer cust1 = new Customer("73838", "Barry", "Bouncealot", new ContactInfo("1244","Burg","MO","67859","6868888","yop@aol.com"));
             Console.WriteLine("\n"+ cust1);
@@ -71,6 +71,11 @@ namespace TesterProgram
             CCA2.IsPastDue = false;
             CCA2.AnnualInterestRate = 4.4m;
             Console.WriteLine("\n"+CCA2);
+
+            Book bk1 = new Book("How to win", "Dale Carnegie", 542);
+            Console.WriteLine("\n" + bk1);
+            Book bk2 = new Book("7 habits", "Stephen Colvey", 833);
+            Console.WriteLine("\n"+bk2);
         }
         
     }
